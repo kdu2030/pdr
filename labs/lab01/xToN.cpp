282be68c76a435416f24451adafa59ebdde2c28e
Kevin Du <kdu2030@gmail.com>
2021-08-27 11:38:10 -0400

    Lab 1

---
+++ labs/lab01/xToN.cpp
@@ -0,0 +1,34 @@
+/*
+ * Name: Kevin Du
+ * ID: kd5eyn
+ * Date: 8/26/2021
+ * File: xToN.cpp
+ */
+
+/*Sources:
+ *https://www.youtube.com/watch?v=vLnPwxZdW4Y&vl=en
+ */
+
+#include <iostream>
+using namespace std;
+
+int xton(int base, int exponent){
+	if(exponent == 0){
+		return 1;
+	}
+	return base * xton(base, exponent-1);
+}
+
+
+int main() {
+	int base = 0;
+	int power = 0;
+	int answer = 0;
+	cout << "Base: ";
+	cin >> base;
+	cout << "Power: ";
+	cin >> power;
+	answer = xton(base, power);
+	cout << "Answer: " << answer << endl;	
+	return 0;
+}
